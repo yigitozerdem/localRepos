539687326550de586611da121485d80ba07713b8
yigito <yigit.ozerdem@sistek.com.tr>
2023-09-04 02:23:18 +0300

    Generated Entity for table.

---
+++ src/main/java/com/integration/fileManagement/entities/FileInformationEntity.java
@@ -0,0 +1,31 @@
+package com.integration.fileManagement.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "files_information")
+public class FileInformationEntity {
+    @Id
+    @SequenceGenerator(
+            name = "file_sequence",
+            sequenceName = "file_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "file_sequence"
+    )
+    private Long id;
+    private String fileName;
+    private String fileSize;
+    private String filePath;
+    private String fileExtension;
+}
