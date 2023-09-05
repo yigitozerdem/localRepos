824762248bdb9f51f6a3dbb8bd12f0a5bf6ba530
yigito <yigit.ozerdem@sistek.com.tr>
2023-09-05 04:25:53 +0300

    Added CorsFilter configuration.

--- src/main/java/com/integration/fileManagement/FileManagementApplication.java
+++ src/main/java/com/integration/fileManagement/FileManagementApplication.java
@@ -1,7 +1,14 @@
 package com.integration.fileManagement;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class FileManagementApplication {
@@ -10,4 +17,19 @@ public class FileManagementApplication {
 		SpringApplication.run(FileManagementApplication.class, args);
 	}
 
+	@Bean
+	public CorsFilter corsConfigurationSource() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin","Content-Type",
+														"Accept","Authorization", "Origin, Accept", "X-Requested-With",
+														"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin","Content-Type",
+				"Accept","Authorization", "Access-Control-Allow-Credentials", "Access-Control-Allow-Origin"));
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsFilter(urlBasedCorsConfigurationSource);
+	}
 }
